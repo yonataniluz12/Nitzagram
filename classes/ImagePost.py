4eeaca453f4c97f28342120c52db05d7e0d6fd97
yonataniluz12 <95865008+yonataniluz12@users.noreply.github.com>
2023-01-29 17:53:30 +0200

    Sher

--- classes/ImagePost.py
+++ classes/ImagePost.py
@@ -10,11 +10,10 @@ class ImagePost(Post):
         Post.__init__(self, location, description)
         self.image_src = image_src
         self.filter = filter
+        img = pygame.image.load(self.image_src)
+        self.image = pygame.transform.scale(img, (POST_WIDTH, POST_HEIGHT))
 
         def display_content(self):
-            img = pygame.image.load(self.image_src)
-            img = pygame.transform.scale(img, (POST_WIDTH, POST_HEIGHT))
-
-            screen.blit(img, (POST_X_POS, POST_Y_POS))
+            screen.blit(self.image, (POST_X_POS, POST_Y_POS))
             if(self.filter != None):
-                self.filter = filter
+                screen.blit(self.filter, (POST_X_POS, POST_Y_POS))
